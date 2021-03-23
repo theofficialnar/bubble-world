1016e960214c19deead76f2393a23553ad72fed2
Nar Cuenca <narc.ph@gmail.com>
2021-03-23 18:22:32 +0800

    add project files

---
+++ canvas.js
@@ -0,0 +1,68 @@
+import Circle from './circle.js';
+
+const canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const ctx = canvas.getContext('2d');
+
+// Adjust canvas size on window resize.
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    initCircles();
+});
+
+let circleArray = [];
+
+/**
+ * Initialize circles.
+ */
+const initCircles = () => {
+    if (circleArray.length) circleArray = [];
+
+    const circlesToGenerate = canvas.width * canvas.height / 5000;
+
+    for (let index = 0; index < circlesToGenerate; index++) {
+        const radius = Math.random() * 5 + 2;
+        let x = Math.random() * (innerWidth - radius * 2) + radius;
+        let dx = (Math.random() - 0.5) * 2;
+        let y = Math.random() * (innerHeight - radius * 2) + radius;
+        let dy = (Math.random() - 0.5) * 2;
+
+        circleArray.push(new Circle(ctx, x, y, dx, dy, radius));
+    }
+}
+
+/**
+ * Draw the text to the canvas.
+ */
+const drawText = () => {
+    ctx.shadowColor = "#000000";
+    ctx.shadowOffsetX = 4;
+    ctx.shadowOffsetY = 4;
+    ctx.shadowBlur = 5;
+    ctx.font = "70px Arial";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.fillText("Bubble World", innerWidth / 2, innerHeight / 2);
+}
+/**
+ * Animate the canvas content.
+ */
+const animate = () => {
+    requestAnimationFrame(animate);
+
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+
+    for (let index = 0; index < circleArray.length; index++) {
+        circleArray[index].update();
+    }
+
+    drawText();
+}
+
+initCircles();
+animate();
---
+++ circle.js
@@ -0,0 +1,74 @@
+const mouse = {
+    x: undefined,
+    y: undefined,
+};
+
+// Set the mouse coordinates whenever it's moved in the canvas.
+window.addEventListener('mousemove', e => {
+    mouse.x = e.x;
+    mouse.y = e.y;
+});
+
+/**
+ * Object to generate a new circle.
+ * @param {Object} ctx Canvas context.
+ * @param {number} x Postion in the X axis
+ * @param {number} y Position in the Y axis
+ * @param {number} dx X axis velocity
+ * @param {number} dy Y axis velocity
+ * @param {number} radius Circle radius.
+ */
+ export default function Circle (ctx, x, y, dx, dy, radius) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+    this.maxRadius = 60;
+    this.minRadius = radius;
+    this.colorArray = [
+        "#8be9fd",
+        "#bd93f9",
+        "#6272a4",
+        "#ff79c6",
+        "#ff5555",
+    ];
+    this.color = this.colorArray[Math.floor(Math.random() * this.colorArray.length)];
+
+    this.draw = () => {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.shadowColor = "#000000";
+        ctx.shadowOffsetX = 6;
+        ctx.shadowOffsetY = 6;
+        ctx.shadowBlur = 20;
+        ctx.fillStyle = this.color;
+        ctx.fill();
+
+    }
+
+    this.update = () => {
+        this.draw();
+
+        if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+            this.dx = -this.dx;
+        }
+
+        if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+            this.dy = -this.dy;
+        }
+
+        this.x += this.dx;
+        this.y += this.dy;
+
+        // interactivity
+        const isInMouseProximity = mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50;
+
+        if (isInMouseProximity) {
+            if (this.radius <= this.maxRadius)
+                this.radius += 2;
+        } else if (this.radius > this.minRadius) {
+            this.radius -= this.minRadius / 3;
+        }
+    }
+}
