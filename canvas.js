b1c243f3436acda341bdd527e2e30afae1e97e2c
Nar Cuenca <narc.ph@gmail.com>
2021-03-23 20:45:05 +0800

    removed shadow blur causing poor performance on mobile

--- canvas.js
+++ canvas.js
@@ -43,7 +43,6 @@ const drawText = () => {
     ctx.shadowColor = "#000000";
     ctx.shadowOffsetX = 4;
     ctx.shadowOffsetY = 4;
-    ctx.shadowBlur = 5;
     ctx.font = "70px Arial";
     ctx.fillStyle = "white";
     ctx.textAlign = "center";
